4fa96f852da52e15146183fb6a92d566e254f005
dilafar <it20784720@my.sliit.lk>
2022-09-03 20:17:04 +0530

    repository, model created for medicine details of child

--- repository/index.js
+++ repository/index.js
@@ -1,3 +1,4 @@
 import { saveUser, loginUser, getUser } from "./user.repository.js";
+import { saveMedicine , updateMedicine , deleteMedicine , getMedicines} from "./medicine.repository.js";
 
 export { saveUser, loginUser, getUser };
