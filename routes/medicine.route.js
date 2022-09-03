8c1d941f25163d916c48bb242cc616668a1d6502
dilafar <it20784720@my.sliit.lk>
2022-09-03 21:38:03 +0530

    Router created for medicine details of child

--- routes/medicine.route.js
+++ routes/medicine.route.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { saveMedicineController, updateMedicineController, deleteMedicineController, getMedicinesController } from "../controllers/index.js";
-import { authenticate } from "../middleware/auth.middleware.js"; 
+import {
+  saveMedicineController,
+  updateMedicineController,
+  deleteMedicineController,
+  getMedicinesController,
+} from "../controllers/index.js";
+import { authenticate } from "../middleware/auth.middleware.js";
 
 const medicineRouter = express.Router();
 
-medicineRouter.post("/add", authenticate , saveMedicineController);
+medicineRouter.post("/add", authenticate, saveMedicineController);
 medicineRouter.delete("/:id", deleteMedicineController);
 medicineRouter.put("/:id", updateMedicineController);
-medicineRouter.get('/', authenticate , getMedicinesController);
+medicineRouter.get("/", authenticate, getMedicinesController);
 
 export default medicineRouter;
