d50ed78c93b0b98fdc7ce7a2cca694416081f61c
dilafar <it20784720@my.sliit.lk>
2022-09-05 00:13:06 +0530

    schedule model  created

--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ import { User } from "./user.model.js";
 import { Event } from "./event.model.js";
 import { Medicine } from "./medicine.model.js";
 import { Child } from "./child.model.js";
+import { Schedule } from "./schedule.model.js";
 
-export { User, Event, Medicine, Child };
+export { User, Event, Medicine, Child, Schedule };
---
+++ models/schedule.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const ScheduleSchema = new Schema(
+  {
+    childID: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    startingTime: {
+      type: String,
+      required: true,
+    },
+    endingTime: {
+      type: String,
+      required: true,
+    },
+    teacherName: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const Schedule = mongoose.model("Schedule", ScheduleSchema);
