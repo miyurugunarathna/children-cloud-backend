0dbf8e834d7b3c2933ee879baa5c5c1b652deb46
dilafar <it20784720@my.sliit.lk>
2022-09-04 19:44:50 +0530

    create child model

---
+++ models/child.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const ChildSchema = new Schema(
+  {
+    parentID: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    dateOfBirt: {
+      type: Date,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    school: {
+      type: String,
+      required: true,
+    },
+    hobby: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const Child = mongoose.model("Child", ChildSchema);
